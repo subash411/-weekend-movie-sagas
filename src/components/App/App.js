8edf0f4598e79ff44363b22a8b5e5a56a75226e4
Subash Khatri <subashkhatri44@gmail.com>
2022-01-29 13:22:34 -0600

    added Home and details on DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,29 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import Details from '../Details/Details';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+
+      <Router> 
+        <nav>
+          <Link className="Link" to="/">Details</Link>
+        </nav> 
+      <Route path="/" exact>
+          <Details />
+
+        </Route>
+
         <Route path="/" exact>
           <MovieList />
         </Route>
+
+        
+
+        
         
         {/* Details page */}
 
