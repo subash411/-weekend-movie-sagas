bb99fc3f773e9a7ec8427b6a7b2c022df19152b5
Subash Khatri <subashkhatri44@gmail.com>
2022-01-30 13:25:16 -0600

    removed soe tables and working on genre

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import {HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
 import Details from '../Details/Details';
